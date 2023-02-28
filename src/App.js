ba360a7a74c0b377ddf113e3fd4bce376b9041e1
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-28 12:11:28 -0500

    successfully api response, console logged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const url = ` https://pokeapi.co/api/v2/pokemon`
+
+axios.get(url).then((response => {
+  console.log(response)
+}));
 
 function App() {
+
   return (
     <div className="App">
       <h1>Pokemon App</h1>
