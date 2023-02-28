75c27eca8afd1296551c131fc9c649daedd8b487
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-28 12:29:44 -0500

    reafacoring of api results using useEffect

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import { useState, useEffect } from 'react';
 
 const url = ` https://pokeapi.co/api/v2/pokemon`
 
-axios.get(url).then((response => {
-  console.log(response)
-}));
-
 function App() {
+
+  const [ pokemon, setPokemon ] = useState([]);
+
+  const getData = async () => {
+    const { data } = await axios.get(url)
+    console.log(data.results)
+    setPokemon(data)
+  };
+
+  useEffect(() => {
+    getData();
+  },[]);
 
   return (
     <div className="App">
-      <h1>Pokemon App</h1>
+      <div></div>
     </div>
   );
 }
